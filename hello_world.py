3e9ebb491c68051229de111263baedf254dc7036
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-03-09 15:18:21 -1000

    Hello World Assignment

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+name = "Reymerson"
+print("Hello", name)
+print("Hello " + name)
+
+number = 16
+print("Hello", number)
+print("Hello " + str(number))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
+name = "Reymerson"
+print(name.upper())
+print(name.lower())
